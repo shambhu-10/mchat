97765f04f0ea770ddd08c5aac81497cf0cbdb625
shambhu <shambhu04.kumar@gmail.com>
2024-12-20 11:20:18 +0530

    Integrated MChat-Backend into the main repository

---
+++ MChat-Backend/index.js
@@ -0,0 +1,39 @@
+const express = require("express");
+const app = express();
+const http = require("http");
+const cors = require("cors");
+const server = http.createServer(app);
+const { Server } = require("socket.io");
+
+app.use(cors());
+
+const io = new Server(server, {
+  cors: {
+    origin: "https://mchatt.netlify.app",
+    methods: ["GET", "POST"],
+  },
+});
+
+io.on("connection", (socket) => {
+  console.log("A user Connected..." + socket.id);
+
+  socket.on("join_room", (data) => {
+    socket.join(data);
+    // socket.emit("receive_users", data.name);
+    console.log(`User with ID: ${socket.id} joined room: ${data}`);
+  });
+
+  socket.on("send_message", (data) => {
+    socket.to(data.room).emit("receive_message", data);
+  });
+
+  socket.on("disconnect", () => {
+    console.log("User Disconnected", socket.id);
+  });
+});
+
+const PORT = process.env.PORT || 5000;
+
+server.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+});
